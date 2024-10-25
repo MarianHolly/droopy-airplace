52d3d62aa68d4b3fc5a7bb096ed1b0c24439dc59
Stupel <marian.holly7117@gmail.com>
2024-10-25 13:25:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame, sys, time 
 from settings import *
+from sprites import DA
 
-class Bird_Game:
+class Droppy_Airplane:
     def __init__(self):
         # setup
         pygame.init() # initialize pygame
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEAIGHT)) # set the screen size
-        pygame.display.set_caption('Flappy Bird') # set the window title
+        pygame.display.set_caption('Droppy Airplane') # set the window title
         self.clock = pygame.time.Clock() # create a clock object
 
         # sprite groups
         self.all_stripes = pygame.sprite.Group() # create a sprite group
-        self.colliion_stripes = pygame.sprite.Group() # create a sprite group
+        self.collision_sprites = pygame.sprite.Group() # create a sprite group
+
+        # scale factor
+        bg_height = pygame.image.load('graphics/environment/background.png').get_height()
+        self.scale_factor = WINDOW_HEAIGHT / bg_height
+
+        # sprites setup
+        DA(self.all_stripes,self.scale_factor)
 
     def run(self):
         last_time = time.time()
@@ -28,9 +36,13 @@ class Bird_Game:
                     sys.exit()
             
             # game logic
+            self.display_surface.fill('black')
+            self.all_stripes.update(dt)
+            self.all_stripes.draw(self.display_surface)
+
             pygame.display.update()
             self.clock.tick(FRAMES_PER_SECOND)
 
 if __name__ == '__main__':
-    game = Bird_Game()
+    game = Droppy_Airplane()
     game.run()
